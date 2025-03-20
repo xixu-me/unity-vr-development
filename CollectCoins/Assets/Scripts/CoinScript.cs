75cf61f5a90e9a6cf22f43596e74e1261cdb990c
Xi Xu <i@xi-xu.me>
2025-03-20 18:49:44 +0800

    Add CollectCoins

---
+++ CollectCoins/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinScript : MonoBehaviour {
+
+	public float rotationSpeed = 100.0f;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Player")
+		{
+			collider.SendMessage("pickup");
+            Destroy(this.gameObject);
+		}
+	}
+}
---
+++ CollectCoins/Assets/Scripts/InventoryScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryScript : MonoBehaviour {
+
+	// Use this for initialization
+	public AudioClip pickupSound;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void pickup()
+	{
+		AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+	}
+}
